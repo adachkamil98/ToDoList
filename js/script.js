9e1e58d5490e5a7624bc3f8a32da2f33b863dc66
adachkamil98 <adachkamil98@gmail.com>
2023-01-17 20:48:38 +0100

    removing tasks added

--- js/script.js
+++ js/script.js
@@ -3,61 +3,81 @@ const welcome = () => {
     console.log("Hello World!")
 };
 
-const tasks = [
-    {
-        content: "umyć naczynia",
-        done: false,
-    },
-    {
-        content: "odkurzyć",
-        done: true,
-    },
-];
+{
 
-const render = () => {
-    let htmlString = "";
+    const tasks = [
+        {
+            content: "umyć naczynia",
+            done: false,
+        },
+        {
+            content: "odkurzyć",
+            done: true,
+        },
+    ];
 
-    for (const task of tasks) {
-        htmlString += `
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
             <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
+            ${task.content}<button class="js-remove">Usuń zadanie</button>
             </li>
         `;
-    }
-
-    document.querySelector(".js-tasksList").innerHTML = htmlString;
-};
-
-const addNewTask = (newTask) => {
-    tasks.unshift(
-        {
-           content: newTask,
         }
-    );
 
-    render();
-}
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
+        const removeButtons = document.querySelectorAll(".js-remove");
 
-    const newTask = document.querySelector(".js-newTaskContent").value.trim();
-    
-    if(newTask === "") {
-        return;
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+    };
+
+    const addNewTask = (newTask) => {
+        tasks.unshift(
+            {
+                content: newTask,
+            }
+        );
+
+        render();
     }
 
-    addNewTask(newTask);
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = document.querySelector(".js-newTaskContent").value.trim();
+
+        if (newTask === "") {
+            return;
+        }
+
+        addNewTask(newTask);
+    }
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+
 }
 
-const init = () => {
-    render();
 
-    const form = document.querySelector(".js-form");
-
-    form.addEventListener("submit", onFormSubmit);
-};
-
-init();
 //const tasksList = document.querySelector(".js-tasksList")
 //tasksList.classList.toggle("section_list--done")
