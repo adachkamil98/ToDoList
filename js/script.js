9db65a478275e8a7d76b182783be8e371567f417
adachkamil98 <adachkamil98@gmail.com>
2023-01-17 18:46:43 +0100

    JS first functions added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log("Hello World!")
+
+const welcome = () => {
+    console.log("Hello World!")
+};
+
+const tasks = [
+    {
+        content: "umyć naczynia",
+        done: false,
+    },
+    {
+        content: "odkurzyć",
+        done: true,
+    },
+];
+
+const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+        htmlString += `
+            <li>
+            ${task.content}
+            </li>
+        `;
+    }
+
+    document.querySelector(".js-tasksList").innerHTML = htmlString;
+};
+
+const init = () => {
+    render();
+};
+
+init();
